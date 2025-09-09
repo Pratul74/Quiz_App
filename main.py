0956a84a3b091edc535ac41a3d89e0b354de7db4
Pratul Kumar <pratulkumar2003@gmail.com>
2025-09-09 16:26:25 +0530

    Added Some functionality

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-class Person:
+from utils import Question, Player, Quiz
+
+def add_
+
+if __name__=="__main__":
+    print("Welcome To My Quiz Game")
     
--- utils.py
+++ utils.py
@@ -9,15 +9,15 @@ class Question:
 
 
 class Player:
-    def __init__(self, name, age):
-        self.name=name
+    def __init__(self, username, age):
+        self.username=username
         self.age=age
+        self.score=0
 
 class Quiz:
     def __init__(self, player):
         self.questions=[]
         self.player=player
-        self.score=0
         self.question_index=0
 
     def add_questions(self, question):
@@ -38,7 +38,7 @@ class Quiz:
         
     def check_answer(self,answer):
         if self.questions[self.question_index].is_correct(answer):
-            self.score+=1
+            self.player.score+=1
             print("Yes, You got the right answer")
             return True
         else:
@@ -48,16 +48,6 @@ class Quiz:
     def __str__(self):
         return f"This Quiz has {len(self.questions)} no. of questions."
     
-question=Question("What is the full form of CPU?",["A.Central Processing unit","B.Central Power Unit","C.Central Machine Unit"],"A")
-question1=Question("2+229=?",["A.231","B.340","C.431"],"A")
-player=Player("Pratul Kumar",22)
-quiz=Quiz(player)
-print(quiz)
-quiz.add_questions(question)
-quiz.add_questions(question1)
-print(quiz.check_answer("A"))
-quiz.get_next_question()
-print(quiz.check_answer("C"))
 
 
 
